780258579f65fb8f0dc649fe3723bbde121f1db6
carlostic26 <carlospenaranda26@gmail.com>
2024-12-16 15:42:10 -0500

    loading screen parte 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:test_1/screens/counter_screen.dart';
+import 'package:test_1/screens/loading_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,12 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const CounterScreen(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const LoadingScreen());
   }
 }
---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double heightScreen = MediaQuery.of(context).size.height;
+    double widthScreen = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Placeholder(
+              child: SizedBox(
+                height: heightScreen * 0.2,
+                width: widthScreen * 0.35,
+                child: Image.asset(fit: BoxFit.fill, 'assets/images/logo.jpg'),
+              ),
+            ),
+            SizedBox(
+              height: heightScreen * 0.02,
+            ),
+            SizedBox(
+              height: heightScreen * 0.03,
+              width: widthScreen * 0.85,
+              child: LinearPercentIndicator(
+                  alignment: MainAxisAlignment.center,
+                  width: widthScreen * 0.65,
+                  lineHeight: 7,
+                  percent: 50 / 100,
+                  animation: true,
+                  animationDuration: 10000,
+                  progressColor: Colors.blueGrey),
+            ),
+            const Text(
+              "Cargando...",
+              textAlign: TextAlign.center,
+              style:
+                  TextStyle(fontSize: 12, fontFamily: 'Inter_ExtraLight.ttf'),
+            ),
+            SizedBox(
+              height: heightScreen * 0.1,
+            ),
+            TextButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor:
+                      WidgetStateProperty.all<Color>(Colors.blueGrey),
+                ),
+                child: const Text(
+                  'Continuar',
+                  style: TextStyle(color: Colors.white),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
