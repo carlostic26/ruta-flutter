f68decf37c2d1f5e3fe9f2e05f38d8fc932d4360
carlostic26 <carlospenaranda26@gmail.com>
2025-04-18 10:58:41 -0500

    solved: bug minSdkversion, bug admob

--- android/app/src/main/kotlin/com/example/test_1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/test_1/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.rutas_flutter
+package com.blogspot.rutaflutter
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:rutas_flutter/core/ads/ads_manager.dart';
 import 'package:rutas_flutter/core/database/database_helper.dart';
 import 'package:rutas_flutter/core/theme/theme_manager.dart';
 import 'package:rutas_flutter/core/theme/theme_notifier.dart';
@@ -9,14 +14,53 @@ import 'package:rutas_flutter/features/loading/presentation/screens/loading_scre
 import 'package:rutas_flutter/features/progress/data/datasources/progress_local_database.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+AppOpenAd? openAd;
+bool isAdLoaded = false;
+
+Future<void> loadOpenAd() async {
+  RutaAdsIds cursinAds = RutaAdsIds();
+  try {
+    await AppOpenAd.load(
+      adUnitId: cursinAds.openApp_adUnitId,
+      request: const AdRequest(),
+      adLoadCallback: AppOpenAdLoadCallback(
+        onAdLoaded: (ad) {
+          openAd = ad;
+          openAd!.show();
+          isAdLoaded = true;
+        },
+        onAdFailedToLoad: (error) {
+          isAdLoaded = false;
+        },
+      ),
+      orientation: AppOpenAd.orientationPortrait,
+    );
+  } catch (e) {
+    //print('Error loading open ad: $e');
+    isAdLoaded = false;
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //await MobileAds.instance.initialize();
   LocalDatabaseHelper().getDatabase();
   ProgressLocalDatabaseHelper().getDatabase;
   LocalExamDataSource().getDatabase;
   LocalExamDataSource().printAllQuestions();
 
+  await dotenv.load(fileName: '.env');
+
   final sharedPreferences = await SharedPreferences.getInstance();
+
+  await loadOpenAd();
+
+  Timer(const Duration(seconds: 12), () async {
+    if (!isAdLoaded) {
+      openAd?.dispose();
+    }
+  });
+
   runApp(ProviderScope(
     overrides: [
       // Sobrescribir el SharedPreferencesProvider con la instancia de SharedPreferences
